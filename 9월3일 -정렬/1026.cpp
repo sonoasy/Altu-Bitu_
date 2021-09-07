b0f24c73b51cf7025d74ccfaab695c6845e36563
sonoasy <61787171+sonoasy@users.noreply.github.com>
2021-09-08 01:24:16 +0900

    [정렬]9월7일

--- 9월3일 -정렬/1026.cpp
+++ 9월3일 -정렬/1026.cpp
@@ -1,15 +1,20 @@
 #include<iostream>
+#include<vector>
 #include<algorithm>
 #define MAX 51
 
 using namespace std;
 
-int a[MAX] = { 0 };
-int b[MAX] = { 0 };
+vector<int> a;
+vector<int> b;
 int main() {
 	//백준 1026번
 	int n;
 	cin >> n;
+
+	a.assign(n,0);
+	b.assign(n, 0);
+
 	for (int i = 0; i < n; i++) {
 		cin >> a[i];
 	}
@@ -17,9 +22,9 @@ int main() {
 		cin >> b[i];
 	}
 	//a,b 각 인덱스의 곱의 합이 최소가 되어야 함
-	sort(a,a+n);
-	sort(b,b+n, greater<>());
-	
+	sort(a.begin(), a.end());
+	sort(b.begin(), b.end(), greater<>());
+
 	int answer = 0;
 	for (int i = 0; i < n; i++) {
 		answer += a[i] * b[i];
