8690eea03d7854b6f20f0e8318040e25d1a2ae22
sonoasy <61787171+sonoasy@users.noreply.github.com>
2021-09-08 02:00:26 +0900

    [정렬]9월8일

--- 9월3일 -정렬/1316.cpp
+++ 9월3일 -정렬/1316.cpp
@@ -6,6 +6,7 @@
 using namespace std;
 int cnt = 0;
 
+bool check[26] = { false };
 int main() {
 	//백준 1316번
 	int n;
@@ -13,33 +14,35 @@ int main() {
 
 	string s;
 	for (int i = 0; i < n; i++) {
-	
-		cin >> s;
-		int flag = 1;
+
+		cin >> s; //문자 입력
+		
+		int flag=1;
+		//check 초기화하기
+		fill_n(check, 26, false);
+		check[s[0] - 'a'] = true; //처음은 일단 체크
 		//단어내 모든 문자에 대해 조사
-		for (int j = 0; j < s.size(); j++) {
-			if (s[j] != s[j + 1]) { //그 다음과 달라서 연속이 아닌데 이후 같은 문자가 나오면 탈락
-				for (int k = j + 2; k <= s.size()-1; k++) {
-					if (s[j] == s[k]) {
-						flag = 0;
-						break;
-					}
-				}
-
-
-			}
-			if (flag == 0) {
+		for (int j = 1; j < s.size(); j++) {
+			//출현했는데 연속이 아닌데 처음 출현하는것도 아니면 탈락
+			if (check[s[j] - 'a'] == true && s[j] != s[j - 1]) {
+				flag = 0;
 				break;
 			}
-
+			else {
+				
+				check[s[j] - 'a'] = true;
+			}
 		}
 		if (flag == 1) {
+			
 			cnt++;
 		}
+		
+
 
 	}
 	cout << cnt;
 
 	return 0;
-	
+
 }
