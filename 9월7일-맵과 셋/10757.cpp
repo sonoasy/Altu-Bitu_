6bb161f989cd4bdcd221c13f27dee438e6c3f909
sonoasy <61787171+sonoasy@users.noreply.github.com>
2021-09-13 18:06:27 +0900

    [맵과 셋]9월13일

--- 9월7일-맵과 셋/10757.cpp
+++ 9월7일-맵과 셋/10757.cpp
@@ -6,60 +6,77 @@
 #define MAX 100000
 using namespace std;
 
-string a;
-string b;
 
 int main() {
 	//백준 10757번
-	
+
 	ios_base::sync_with_stdio(0);
 	cin.tie(0), cout.tie(0);
+	string a;
+	string b;
 
-	
+
 	cin >> a >> b;
 
-	//순서가 거꾸로 되있으니까 뒤집기
-	reverse(a.begin(),a.end());   //일의자리수가 0인덱스부터 있음
-	reverse(b.begin(), b.end());
-
-	//자리수 큰게 a로 하기
-	if (a.size() < b.size()) {
-		swap(a, b);
-	}
-	//a크기만큼 b에 0으로 채우기
-	for (int i = b.size(); i < a.size(); i++) {
-		b += "0";
-	}
-	
 	//답 저장
 	string answer;
 
 	//더해서 10이 넘어서 넘길 1 체크
 	int flag = 0;
-	for (int i = 0; i<a.size(); i++) {
+	int check;
+	//인덱스 끝부터 시작하기
+	int idxa = a.size() - 1;
+	int idxb = b.size() - 1;
+	int suma, sumb;
+	//둘중 하나는 글자인 숫자가 있을떄까지 계속하기
+	while (idxa >= 0 || idxb >= 0) {
 		//큰자리수 끝에 맞춰서 
-		int num = (a[i] - '0') +(b[i] - '0');
+
 		
+		if (idxa >= 0) {
+			suma = (a[idxa] - '0');
+		}
+		if (idxb >= 0) {
+			sumb = (b[idxb] - '0');
+		}
+		
+
+		//만약 한쪽이 공백이어서 음수가 되면 0으로 바꾸기
+		if (suma < 0 ||idxa<0) {
+			suma = 0;
+		}
+		if (sumb < 0 || idxb < 0) {
+			sumb = 0;
+		}
+
+		int num = suma + sumb;
+
+		//올라온 1이 있으면 더해주기
 		if (flag == 1) {
 			num += flag;
 			flag = 0;
 		}
+		//합이 10이 넘으면 넘길 1 저장하기
 		if (num >= 10) {
 			num %= 10;
 			flag = 1;
 		}
-		answer+=to_string(num);
-
+		//string으로 바꿔서 저장하기
+		answer += to_string(num);
+		
+		idxa--;
+		idxb--;
+		
 
 	}
-	
+
 	//flag가 남았으면 추가해주기
 	if (flag == 1) {
-		answer+="1";
+		answer += "1";
 	}
 
 	//다시 뒤집기
-	reverse(answer.begin(),answer.end());
+	reverse(answer.begin(), answer.end());
 
 	cout << answer;
 
