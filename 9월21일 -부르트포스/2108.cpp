47f08cd8606542ddee82ee45af2985994b98f1d0
sonoasy <61787171+sonoasy@users.noreply.github.com>
2021-09-27 22:44:11 +0900

    [부르트포스]9월27일

--- 9월21일 -부르트포스/2108.cpp
+++ 9월21일 -부르트포스/2108.cpp
@@ -8,70 +8,121 @@
 using namespace std;
 
 vector<int> num;
-map<int, int>freq;
+int arr[8002] = { 0 };
 
-bool cmp(const pair<int, int>& a, const pair<int, int>& b) {
-    if (a.second == b.second) return a.first < b.first;
-    return a.second < b.second;
+//산술 평균 구하기
+double avg(int n, vector<int> v) {
+    double sum = 0;
+    for (int i = 0; i < v.size(); i++) {
+        sum += v[i];
+    }
+    return round(sum / n);
+
 }
 
+
+
+//중앙값 출력하기
+int mid(int n, vector<int>v) {
+    //n은 홀수라고 가정하므로 n/2번째를 탐색하면됨
+   
+    return v[n/2];
+}
+
+
+
+//최빈값 
+int freqs(int n, vector<int>v) {
+   //입력되는 정수의 절댓값은 4000을 넘지 않음
+   //-4000부터 4000까지 값에 해당되면 +1 하기
+     //0     1     3999 4000 4001       8000    arr 인덱스  
+    //-4000,-3999,..-1,  0, 1 ,...3999,4000     매칭되는 숫자
+
+    for (int i = 0; i < v.size(); i++) {
+        //s는 숫자
+        int s = v[i];
+        arr[s+4000]++;
+    }
+
+    int max = 0;
+    int maxi;
+    //arr에서 최빈값을 구하기
+    for (int i = 0; i < 8002; i++) {
+        if (max <= arr[i]) { //여러개일수 있으므로 같거나 큰 값을 탐색
+            max = arr[i];
+            //마지막 최빈값 인덱스
+            maxi = i;
+        }
+    }
+    
+    //최빈값에 해당되는게 여러개인지 확인하기
+    int flags = 0;
+    for (int i = 0; i < 8002; i++) {
+        //max값인데 maxi와 인덱스가 다르다면 여러개
+        if (arr[i] == max && i != maxi) {
+            flags = 1;
+            break;
+        }
+
+    }
+    //여러개일때
+    if (flags == 1) {
+        // 이미 숫자는 오름차순이므로 maxi 보다 작은 숫자들 중 두번쨰로 작은 숫자를 출력
+        int count = 0;
+        for (int i = 0; i < maxi; i++) {
+            if (arr[i] == max) {
+                count++;
+            }
+            
+            if (count == 2) {
+                //실제 숫자
+                return i-4000;
+            }
+        }
+
+
+    }
+   
+    return maxi-4000;
+   
+
+
+}
+
+
+//범위 출력
+int range(int n, vector<int>v) {
+
+    return v[v.size() - 1] - v[0];
+}
+
+
 int main() {
- 
+
     //백준 2108번
-    int n,m;
+    int n, m;
     double sum = 0;
     cin >> n;
     for (int i = 0; i < n; i++) {
         cin >> m;
         sum += m;
-        freq[m]++;
         num.push_back(m);
     }
-    //산술평균 반올림
-    cout << round(sum / n)<<'\n';
+    //산술평균 반올림하여 출력하기
+    cout << avg(n,num) << '\n';
 
-    //중앙값
+
+    //중앙값 계산부터는 정렬된 벡터를 사용
     sort(num.begin(), num.end());
-    int s;
-    if (n % 2 == 0) {
-        s = n / 2;
-    }
-    else {
-        s = n / 2 + 1;
-    }
 
-    cout << num[s - 1] << '\n';
 
-    //최빈값 여러개 있을떄는 두번쨰로 작은 값
-    //빈도수에 맞춰 오름차순 정렬하기
-    vector<pair<int, int>> vec(freq.begin(), freq.end());
-    sort(vec.begin(), vec.end(), cmp);
-
-    //여러개 있을때 
-    int max = vec[vec.size() - 1].second;
-    int flag = 0;
-    vector<int>check;
-    for (int i = 0; i < vec.size()-1; i++) {
-        if (vec[i].second == max) {
-            flag = 1;
-            check.push_back(vec[i].first);
-        }
-
-    }
-    check.push_back(vec[vec.size() - 1].first);
-    if (flag == 1) {
-        sort(check.begin(), check.end());
-        cout << check[1] << '\n';
-
-    }
-    else {
-        cout << vec[vec.size() - 1].first << '\n';
-    }
-   
-
-    //범위
-    cout << num[n - 1] - num[0] << '\n';
-
+    //중앙값 출력하기
+    cout << mid(n,num) << '\n';
+    //최빈값
+    cout << freqs(n, num) << '\n';
+  
+    //범위 출력하기
+    cout << range(n,num) << '\n';
 
 
 
