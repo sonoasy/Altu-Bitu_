f15073ca8f8a4f11b56ab65ab4baee10f7ee5874
sonoasy <61787171+sonoasy@users.noreply.github.com>
2021-09-27 21:28:45 +0900

    [부르트포스]9월27일

--- 9월21일 -부르트포스/10448.cpp
+++ 9월21일 -부르트포스/10448.cpp
@@ -4,30 +4,38 @@
 using namespace std;
 
 int main() {
- 
+
     //백준 10448번
     //삼각수는 n*(n+1)/2 
     //3개의 삼각수로 표현이 되는지 확인
     int n;
     cin >> n;
+
+    //n 삼각수 설정하기
+    int tri[1001];
+    for (int i = 1; i <= 1000; i++) {
+        tri[i] = i * (i + 1) / 2;
+    }
+
     while (n--) {
         int m;
         cin >> m;
 
-        //시간초과
+        
         int sum = 0;
         int flag = 0;
-
+        //시간초과 방지 
         double s = sqrt(2 * m);
         //숫자가 다를 필요없음!!
         for (int i = 1; i < s; i++) {
             for (int j = 1; j < s; j++) {
                 for (int k = 1; k < s; k++) {
                     sum = 0;
-                    sum += (i * (i + 1) / 2) + (j * (j + 1) / 2) +( k * (k + 1) / 2);
+                    sum += tri[i] + tri[j] + tri[k];
+                    //세 삼각수 합이 입력값과 같으면 flag 1 표기
                     if (sum == m) {
                         flag = 1;
-                        
+
                         break;
                     }
 
@@ -39,19 +47,12 @@ int main() {
             cout << 0 << '\n';
         }
         else {
+            //flag가 1일떄만 1 출력
             cout << 1 << '\n';
         }
 
     }
 
-   
 
-
-
-
-
-
-
-  
     return 0;
 }
