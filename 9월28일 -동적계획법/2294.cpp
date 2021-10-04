82f19401144de8f478034118ace09a94d77ebd0f
sonoasy <61787171+sonoasy@users.noreply.github.com>
2021-10-04 09:57:39 +0900

    [동적계획벅]10월4일

---
+++ 9월28일 -동적계획법/2294.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include<vector>
+
+using namespace std;
+
+int main() {
+	//백준 2294번
+	
+	ios_base::sync_with_stdio(0);
+	cin.tie(0), cout.tie(0);
+
+	//가치합이 k가 되도록, 동전은 여러번 사용 가능
+
+	int n, k;
+	cin >> n >> k;
+	vector<int>coin(101, 0);
+
+	for (int i=0; i < n; i++) {
+		cin >> coin[i];
+	}
+	vector<int>dp(100001, 100001);
+	//합이 0이되는 경우는 0
+	dp[0]=0;
+	for (int i = 0; i < n; i++) {
+		//동전을 포함하는 경우 
+		//동전을 여러번 사용해도 되므로 갱신이 될수 있을때마다 최솟값으로 갱신 
+		for (int j = coin[i]; j <= k; j++) {
+			dp[j] = min(dp[j], dp[j - coin[i]] + 1);
+		}
+		
+	}
+	//처음 초기화값과 달라지는게 없으면 -1출력
+	if (dp[k] == 100001) {
+		cout << -1;
+	}
+	else {
+		cout << dp[k] << endl;
+	}
+	
+	
+
+}
