14e80cb11409a10a35d708c129a55bd5c461e1bb
sonoasy <61787171+sonoasy@users.noreply.github.com>
2021-10-04 23:51:36 +0900

    [동적계획법]--ing

--- 9월28일 -동적계획법/20923.cpp
+++ 9월28일 -동적계획법/20923.cpp
@@ -12,39 +12,57 @@ int cnt = 0;
 //종을 치는 조건
 void ring() {
 
-	
-    //수연: 그라운드 맨위에 있는 숫자 합이 5일때(어느쪽도 비지 않을때)
+	if (doo.empty()) {
+		
+		return;
+	}
+	if (suu.empty()) {
+		
+		return;
+	}
+
+	//수연: 그라운드 맨위에 있는 숫자 합이 5일때(어느쪽도 비지 않을때)
 	if (!doo_ground.empty() && !suu_ground.empty()) {
 		//도도,수연 각각 그라운드 맨위의 카드 합이 5이면 종을 침
 		if (doo_ground.back() + suu_ground.back() == 5) {
 			//상대방 그라운드에 있는 카드 더미를 뒤집어서 자신의 덱 아래로 넣기
-			
-			while(!doo_ground.empty()) {
+
+			while (!doo_ground.empty()) {
 				suu.push_front(doo_ground.front());
 				doo_ground.pop_front();
 			}
-			//게임 1번 끝남
-			cnt++;
-		}
-
-	}
-	
-
-	//도도: 그라운드 맨위에 있는 숫자가 5가 나타날때
-	if (!doo_ground.empty() && !suu_ground.empty()) {
-		if (doo_ground.back() == 5 || suu_ground.back() == 5) {
-			//상대방 그라운드에 있는 카드 더미를 뒤집어서 자신의 덱 아래로 넣기
-
+			//자신의 그라운드 더미도 뒤집어서 자신의 덱 밑으로
 			while (!suu_ground.empty()) {
-				doo.push_front(suu_ground.front());
+				suu.push_front(suu_ground.front());
 				suu_ground.pop_front();
 			}
-			//게임 한번 끝남
-			cnt++;
+
+
+		
 		}
 
 	}
-	
+
+
+	//도도: 그라운드 맨위에 있는 숫자가 5가 나타날때
+
+	if ((!doo_ground.empty() && doo_ground.back() == 5) || (!suu_ground.empty() && suu_ground.back() == 5)) {
+		//상대방 그라운드에 있는 카드 더미를 뒤집어서 자신의 덱 아래로 넣기
+
+		while (!suu_ground.empty()) {
+			doo.push_front(suu_ground.front());
+			suu_ground.pop_front();
+		}
+		//자신의 그라운드 더미도 뒤집어서 자신의 덱 밑으로
+		while (!doo_ground.empty()) {
+			doo.push_front(doo_ground.front());
+			doo_ground.pop_front();
+		}
+
+		
+	}
+
+
 
 }
 
@@ -64,45 +82,39 @@ int main() {
 		suu.push_back(s);
 	}
 
+
 	//deque 맨뒤에서부터 그라운드에 올려놓음
-	while (1) {
+	while (m--) {
 		//도도 덱에 카드가 있으면 도도 그라운드에 올려놓기
 		if (!doo.empty()) {
 			doo_ground.push_back(doo.back());
 			doo.pop_back();
+		
+			//덱이 비었으면 상대방이 승리
+			if (doo.empty()) { //수연이 승리
+				break;
+
+			}
 		}
 
 		//종울리수 있는지 확인
 		ring();
 
-		
+
 		//수연 덱에 카드가 있으면 수연 그라운드에 올려놓기
 		if (!suu.empty()) {
 			suu_ground.push_back(suu.back());
 			suu.pop_back();
+			
+
+			//덱이 비었으면
+			if (suu.empty()) {//도도가 승리
+				break;
+			}
 		}
 		//종 울리수 있는지 확인
 		ring();
 
-		//m번 진행했으면 종료
-		if (cnt == m)break;
-		//덱이 비었으면 상대방이 승리
-		if (doo.empty()) { //수연이 승리
-			while (!doo_ground.empty()) {
-				suu.push_front(doo_ground.front());
-				doo_ground.pop_front();
-			}
-			cnt++;
-
-		}
-		if (suu.empty()) {//도도가 승리
-			while (!suu_ground.empty()) {
-				doo.push_front(suu_ground.front());
-				suu_ground.pop_front();
-			}
-			cnt++;
-		}
-	
 
 	}
 
