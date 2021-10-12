7c64c36b0a320744d49e888ba2c2e2b1416cfbad
sonoasy <61787171+sonoasy@users.noreply.github.com>
2021-10-12 15:17:16 +0900

    [백트래킹]10월12일

--- 10월 5일 - 분할 정복/16198.cpp
+++ 10월 5일 - 분할 정복/16198.cpp
@@ -1,70 +1,48 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
 
 using namespace std;
 
-int res = -1;
-int check[11] = { 0 };
-vector<int>energy(11, 0);
-void go(vector<int> v,int s) {
-    
-    if (v.size() == 2) { //두개가 남으면 더이상 선택할수 없음
-        if (res < s) {
-            res = s;
-        }
-        return;
+vector<int> w; //에너지 값저장
+int ans; //결과 
+
+void backtracking(int sum) {
+    if (w.size() == 2) { //에너지 구슬이 2개 -> 더 이상 고를 수 없음
+        ans = max(ans, sum); //기존 갱신된 ans과 sum 중에 더 큰값을 ans로 설정 
+        return; //종료
     }
-    //2부터 v.size()-2까지 선택 가능
-    for (int i = 2; i <= v.size() - 2; i++) {
-        //에너지값도 초기화해줘야함
-        int tmp = s;
-        tmp += v[i - 1] * v[i + 1];
-        //energy가 변동되면 안됨!!!!
-        vector<int>q = v;
-        q.erase(q.begin() + i); //1 2 4
-        //for (int i = 0; i < q.size(); i++) {
-       //    cout << q[i] << ' ';
-       // }
-        go(q, tmp);
-        
+    //0부터 저장되어있음 
+    for (int i = 1; i < w.size() - 1; i++) { //처음과 마지막 선택 불가이므로
+        int temp_w = w[i]; //선택한 인덱스의 에너지 값
+        int sum_w = w[i - 1] * w[i + 1]; //선택한 인덱스 전,후로의 에너지값을 곱해서 더하기
+        w.erase(w.begin() + i); //w에서 선택한 인덱스 지우기
+        backtracking(sum + sum_w); //최종 합에 전,후로의 에너지값 곱한것 더해주기
+        w.insert(w.begin() + i, temp_w); //다른 경우도 w사용하므로 다시 지운 인덱스의 값 저장해주기
     }
-
-
-
 }
 
-
+/**
+ * 가능한 모든 경우를 구해서 계산하자
+ * 이때, 사용한 에너지는 따로 저장해 둔 후에 배열에서 지우고, 다시 돌아왔을 때 저장한 값을 배열에 넣는다.
+ */
 int main() {
-   
-    //백준 16198번
 
+    //에너지 갯수
     int n;
+
+    //입력
     cin >> n;
-    
-    
-    //i번째 구슬의 무게:energy[i]
-    for (int i = 1; i <= n; i++) {
-        cin >> energy[i];
-    }
+    //애너지 n개에 대하여 
+    w.assign(n, 0);
+    //각각 에너지의
+    for (int i = 0; i < n; i++)
+        cin >> w[i]; //크기를 저장한다.
 
-    //에너지를 모으는 방법 
-    //에너지 x번째 구슬을 하나 고른다.(1,n번째 제외) 고른 구슬 제거
-    // energy[x-1]*energy[x+1] 에너지 모음
-    // n 제거 후 구슬 1부터 번호 다시 매김 
+    //연산
+    backtracking(0);
 
-    //1 2 3 4     -> 1 2 4
-    // 3 제거 ->8 , 2 제거 -> 4 = 12
-    //줄어든 벡터 어떻게 처리 -> erase 사용
-    int w = 0; //에너지 초기값
-   
-    go(energy,w);
-    
-
-
-    cout << res;
-
-
+    //출력
+    cout << ans << '\n';
 
     return 0;
 }
