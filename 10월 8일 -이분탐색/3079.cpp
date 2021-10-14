480d3b9bc3f37cf05d1ae75c2e5e3422800e0272
sonoasy <61787171+sonoasy@users.noreply.github.com>
2021-10-14 23:36:56 +0900

    [이분탐색]10월14일

---
+++ 10월 8일 -이분탐색/3079.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include<climits>
+using namespace std;
+
+vector<long long>v;
+int main() {
+   
+	//백준 3079번
+
+	long long n, m; //입국 심사대, 친구 m명
+	
+
+	cin >> n >> m;
+	
+	long long a;
+	for (int i = 0; i < n; i++) {
+		cin >> a;
+		v.push_back(a);  //걸리는 시간
+		
+	}
+
+	long long ans = LLONG_MAX;
+	sort(v.begin(), v.end());
+
+	long long start = 1;
+	long long end = m * v[v.size() - 1];  //최대 m명 최대시간으로 곱하기 
+
+	long long mid=0;
+	long long sum=0;
+	while (start <= end) {
+
+		mid = (start + end) / 2;
+		sum = 0;
+		for (int i = 0; i < v.size(); i++) {
+			sum+=mid / v[i];
+
+		}
+		
+		if (sum >= m) { //m명을 만족하면 
+			ans = min(ans,mid); //저장 
+			end = mid - 1;
+			
+		}
+		else {
+			start = mid + 1;
+		}
+	}
+	cout << ans;
+
+
+
+}
